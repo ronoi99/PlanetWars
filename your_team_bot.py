0a45e917f992303cd3d0c7cdb89b9644d2f63760
ron wett <ronoi99@gmail.com>
2022-02-18 18:44:43 +0200

    your team bot file

---
+++ your_team_bot.py
@@ -0,0 +1,71 @@
+import random
+from typing import Iterable, List
+
+from baseline_bot import AttackWeakestPlanetFromStrongestBot, AttackEnemyWeakestPlanetFromStrongestBot, \
+    AttackWeakestPlanetFromStrongestSmarterNumOfShipsBot, get_random_map
+from planet_wars.planet_wars import Player, PlanetWars, Order, Planet
+from planet_wars.battles.tournament import get_map_by_id, run_and_view_battle, TestBot
+
+import pandas as pd
+
+
+class YourCoolBot(Player):
+    """
+    Implement here your smart logic.
+    Rename the class and the module to your team name
+    """
+
+    def play_turn(self, game: PlanetWars) -> Iterable[Order]:
+        """
+        See player.play_turn documentation.
+        :param game: PlanetWars object representing the map - use it to fetch all the planets and flees in the map.
+        :return: List of orders to execute, each order sends ship from a planet I own to other planet.
+        """
+        # TODO IMPLEMENT HERE YOUR SMART LOGIC
+        pass
+
+
+def view_bots_battle():
+    """
+    Runs a battle and show the results in the Java viewer
+
+    Note: The viewer can only open one battle at a time - so before viewing new battle close the window of the
+    previous one.
+    Requirements: Java should be installed on your device.
+    """
+    map_str = get_random_map()
+    run_and_view_battle(AttackWeakestPlanetFromStrongestBot(), AttackEnemyWeakestPlanetFromStrongestBot(), map_str)
+
+
+def check_bot():
+    """
+    Test AttackWeakestPlanetFromStrongestBot against the 2 other bots.
+    Print the battle results data frame and the PlayerScore object of the tested bot.
+    So is AttackWeakestPlanetFromStrongestBot worse than the 2 other bots? The answer might surprise you.
+    """
+    maps = [get_random_map(), get_random_map()]
+    player_bot_to_test = AttackWeakestPlanetFromStrongestBot()
+    tester = TestBot(
+        player=player_bot_to_test,
+        competitors=[
+            AttackEnemyWeakestPlanetFromStrongestBot(), AttackWeakestPlanetFromStrongestSmarterNumOfShipsBot()
+        ],
+        maps=maps
+    )
+    tester.run_tournament()
+
+    # for a nicer df printing
+    pd.set_option('display.max_columns', 30)
+    pd.set_option('expand_frame_repr', False)
+
+    print(tester.get_testing_results_data_frame())
+    print("\n\n")
+    print(tester.get_score_object())
+
+    # To view battle number 4 uncomment the line below
+    # tester.view_battle(4)
+
+
+if __name__ == "__main__":
+    # check_bot()
+    view_bots_battle()
