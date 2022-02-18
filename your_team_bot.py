a5c165be254e4b1ea0f5818ab38c5fceffc1c366
ron wett <ronoi99@gmail.com>
2022-02-18 21:44:59 +0200

    MY COOL BOT :)

--- your_team_bot.py
+++ your_team_bot.py
@@ -2,17 +2,92 @@ from typing import Iterable
 
 import pandas as pd
 
-from baseline_bot import AttackWeakestPlanetFromStrongestBot, AttackEnemyWeakestPlanetFromStrongestBot, \
-    AttackWeakestPlanetFromStrongestSmarterNumOfShipsBot, get_random_map
+from baseline_bot import AttackWeakestPlanetFromStrongestBot, get_random_map
 from planet_wars.battles.tournament import run_and_view_battle, TestBot
-from planet_wars.planet_wars import Player, PlanetWars, Order, Planet
+from planet_wars.planet_wars import Player, PlanetWars, Order, Planet, Fleet
 
 
-class YourCoolBot(Player):
+class PlanetToAttack:
+    def __init__(self, planet: Planet, needed_ships: int, score: float):
+        self.planet = planet
+        self.needed_ships = needed_ships
+        self.score = score
+
+
+class RonCoolBot(Player):
     """
     Implement here your smart logic.
     Rename the class and the module to your team name
     """
+    @staticmethod
+    def fight(neutral, enemy, me):
+        if neutral >= enemy and neutral >= me:
+            return PlanetWars.NEUTRAL, neutral - max(enemy, me)
+        if enemy >= neutral and enemy >= me:
+            return PlanetWars.ENEMY, enemy - max(neutral, me)
+        if me >= enemy and me >= neutral:
+            return PlanetWars.ME, me - max(enemy, neutral)
+
+    @classmethod
+    def ships_in_turn(cls, planet: Planet, turn: int, game: PlanetWars):
+        """
+        :return: The amount of ships on the given planet in 'turn' turns
+        """
+        fleets = [f for f in game.fleets if f.destination_planet_id == planet.planet_id]
+        if len(fleets) == 0:
+            growth_rate = planet.growth_rate if planet.owner == PlanetWars.ENEMY else 0
+            return planet.owner, planet.num_ships + growth_rate * turn
+
+        # simulate the fleets
+        number_of_ships = planet.num_ships
+        current_owner = planet.owner
+        for time in range(1, turn+1):
+            growth_rate = planet.growth_rate if current_owner != PlanetWars.NEUTRAL else 0
+            number_of_ships += growth_rate
+            arriving_fleets = [f for f in fleets if f.turns_remaining == time]
+            if len(arriving_fleets) == 0:
+                continue
+            enemy_ships = sum([f.num_ships for f in arriving_fleets if f.owner == PlanetWars.ENEMY] or [0])
+            my_ships = sum([f.num_ships for f in arriving_fleets if f.owner == PlanetWars.ME] or [0])
+            neutral_ships = 0
+            if current_owner == PlanetWars.NEUTRAL:
+                neutral_ships += number_of_ships
+            elif current_owner == PlanetWars.ENEMY:
+                enemy_ships += number_of_ships
+            elif current_owner == PlanetWars.ME:
+                my_ships += number_of_ships
+
+            current_owner, number_of_ships = cls.fight(neutral_ships, enemy_ships, my_ships)
+
+        return current_owner, number_of_ships
+
+    @classmethod
+    def create_planet_to_attack(cls, source_planet: Planet, dest_planet: Planet, game: PlanetWars):
+        distance = Planet.distance_between_planets(source_planet, dest_planet)
+        current_owner, number_of_ships = cls.ships_in_turn(dest_planet, turn=distance+1, game=game)
+        factor = 0
+        if current_owner == PlanetWars.ENEMY:
+            factor = 2
+        elif current_owner == PlanetWars.NEUTRAL:
+            factor = 1
+        ships = number_of_ships
+        if distance < 10:
+            ships -= (10 - distance) * dest_planet.growth_rate
+        ships = ships if ships > 0 else 1
+        score = (dest_planet.growth_rate * factor) / ships
+        return PlanetToAttack(dest_planet, number_of_ships, score)
+
+    @staticmethod
+    def planet_score(planet: Planet, number_of_ships_at_arrival: int):
+        return (planet.growth_rate * (1 if planet.owner == PlanetWars.NEUTRAL else 2)) / number_of_ships_at_arrival
+
+    def planet_we_can_attack_from(self, planet: Planet, game: PlanetWars):
+        if planet.num_ships < 10:
+            return False
+        owner, num_of_ships = self.ships_in_turn(planet, turn=20, game=game)
+        if owner != PlanetWars.ME or num_of_ships < 10:
+            return False
+        return True
 
     def play_turn(self, game: PlanetWars) -> Iterable[Order]:
         """
@@ -20,8 +95,32 @@ class YourCoolBot(Player):
         :param game: PlanetWars object representing the map - use it to fetch all the planets and flees in the map.
         :return: List of orders to execute, each order sends ship from a planet I own to other planet.
         """
-        # TODO IMPLEMENT HERE YOUR SMART LOGIC
-        pass
+        orders = []
+        for source_planet in game.get_planets_by_owner(PlanetWars.ME):
+            if not self.planet_we_can_attack_from(source_planet, game):
+                continue
+
+            planets_to_attack = [self.create_planet_to_attack(source_planet, p, game) for p in game.planets]
+            planets_to_attack = sorted(planets_to_attack, key=lambda p: p.score, reverse=True)
+            source_planet_ships = source_planet.num_ships
+            for planet_to_attack in planets_to_attack:
+                needed_ships = planet_to_attack.needed_ships+5
+                source_planet.num_ships -= needed_ships
+                if not self.planet_we_can_attack_from(source_planet, game):
+                    break
+                orders.append(Order(source_planet, planet_to_attack.planet, needed_ships))
+
+                distance = Planet.distance_between_planets(source_planet, planet_to_attack.planet)
+                game.fleets.append(
+                    Fleet(
+                        owner=PlanetWars.ME, num_ships=needed_ships, source_planet_id=source_planet.planet_id,
+                        destination_planet_id=planet_to_attack.planet.planet_id,
+                        total_trip_length=distance, turns_remaining=distance
+                    )
+                )
+            source_planet.num_ships = source_planet_ships
+
+        return orders
 
 
 def view_bots_battle():
@@ -33,7 +132,7 @@ def view_bots_battle():
     Requirements: Java should be installed on your device.
     """
     map_str = get_random_map()
-    run_and_view_battle(AttackWeakestPlanetFromStrongestBot(), AttackEnemyWeakestPlanetFromStrongestBot(), map_str)
+    run_and_view_battle(AttackWeakestPlanetFromStrongestBot(), RonCoolBot(), map_str)
 
 
 def check_bot():
@@ -42,12 +141,12 @@ def check_bot():
     Print the battle results data frame and the PlayerScore object of the tested bot.
     So is AttackWeakestPlanetFromStrongestBot worse than the 2 other bots? The answer might surprise you.
     """
-    maps = [get_random_map(), get_random_map()]
-    player_bot_to_test = AttackWeakestPlanetFromStrongestBot()
+    maps = [get_random_map() for i in range(20)]
+    player_bot_to_test = RonCoolBot()
     tester = TestBot(
         player=player_bot_to_test,
         competitors=[
-            AttackEnemyWeakestPlanetFromStrongestBot(), AttackWeakestPlanetFromStrongestSmarterNumOfShipsBot()
+            AttackWeakestPlanetFromStrongestBot()
         ],
         maps=maps
     )
@@ -66,5 +165,5 @@ def check_bot():
 
 
 if __name__ == "__main__":
-    # check_bot()
-    view_bots_battle()
+    check_bot()
+    # view_bots_battle()
